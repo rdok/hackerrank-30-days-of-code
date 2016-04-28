4cf4f948c2cdb55e2f426c73083d0b02a4e6f039
Rizart Dokollari <r.dokollari@gmail.com>
2016-04-28 12:01:30 +0000

    Assert it tells whether a person is young, teenager, or old.

--- 04-class-vs-instance/app/Person.php
+++ 04-class-vs-instance/app/Person.php
@@ -6,8 +6,15 @@
 
 namespace App;
 
+/**
+ * Class Person
+ * @package App
+ */
 class Person
 {
+    /**
+     * @var
+     */
     private $age;
 
     /**
@@ -19,11 +26,17 @@ class Person
         $this->setAge($initialAge);
     }
 
+    /**
+     *
+     */
     public function yearPasses()
     {
         $this->setAge($this->getAge() + 1);
     }
 
+    /**
+     * @return mixed
+     */
     public function getAge()
     {
         return $this->age;
@@ -44,5 +57,21 @@ class Person
         $this->age = $age;
 
         return null;
+    }
+
+    /**
+     * @return string
+     */
+    public function amIOld()
+    {
+        if ($this->getAge() < 13) {
+            return 'You are young.';
+        }
+
+        if ($this->getAge() < 18) {
+            return 'You are a teenager.';
+        }
+
+        return 'You are old.';
     }
 }
--- 04-class-vs-instance/tests/PersonTest.php
+++ 04-class-vs-instance/tests/PersonTest.php
@@ -21,9 +21,7 @@ class PersonTest extends PHPUnit_Framework_TestCase
     /** @test */
     public function it_increases_age_each_year()
     {
-        $incorrectAge = -1;
         $person = new Person();
-
         $this->assertSame(0, $person->getAge());
 
         $person->yearPasses();
@@ -31,5 +29,32 @@ class PersonTest extends PHPUnit_Framework_TestCase
 
         $person->yearPasses();
         $this->assertSame(2, $person->getAge());
+    }
+
+    /** @test */
+    public function it_returns_whether_a_person_is_young_teenager_or_old()
+    {
+        $person = new Person();
+        $this->assertSame(0, $person->getAge());
+
+        $this->assertSame('You are young.', $person->amIOld());
+        $person->setAge(7);
+        $this->assertSame('You are young.', $person->amIOld());
+        $person->setAge(12);
+        $this->assertSame('You are young.', $person->amIOld());
+
+        $person->setAge(13);
+        $this->assertSame('You are a teenager.', $person->amIOld());
+        $person->setAge(15);
+        $this->assertSame('You are a teenager.', $person->amIOld());
+        $person->setAge(17);
+        $this->assertSame('You are a teenager.', $person->amIOld());
+
+        $person->setAge(18);
+        $this->assertSame('You are old.', $person->amIOld());
+        $person->setAge(25);
+        $this->assertSame('You are old.', $person->amIOld());
+        $person->setAge(75);
+        $this->assertSame('You are old.', $person->amIOld());
     }
 }
