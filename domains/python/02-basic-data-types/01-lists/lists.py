c8beaa56f6c5d1e8167b6512287d3f54bed95397
Rizart Dokollari <r.dokollari@gmail.com>
2017-04-29 06:58:09 +0300

    Python lists. See https://trello.com/c/yG1gfYJf

--- domains/python/02-basic-data-types/01-lists/lists.py
+++ domains/python/02-basic-data-types/01-lists/lists.py
@@ -1,17 +1,52 @@
+import sys
+lists = []
+
 def executeCommands():
     numberOfCommands = int( raw_input() )
-
-    if( numberOfCommands == 0):
-        return None
-
-    lists = []
+    availableCommands = {
+        'print': printList,
+        'insert': insertList,
+        'remove': removeList,
+        'append': appendList,
+        'sort': sortList,
+        'pop': popList,
+        'reverse': reverseList
+    }
 
     for commandIndex in range(0, numberOfCommands):
         rawCommand = raw_input().split(' ')
-        lists.rawCommand[0]()
+        
+        if not ( rawCommand[1:] ) :
+            availableCommands[ rawCommand[0] ]()
+        else:
+            arguments = rawCommand[1:]
+            availableCommands[ rawCommand[0] ]( *arguments )
 
-
+def printList(): 
     print lists
+
+def insertList(index, value): 
+    index = int( index )
+    value = int( value )
+
+    lists.insert(index, value)
+
+def removeList(value): 
+    number = int( value )
+    lists.remove(number)
+
+def appendList(value): 
+    number = int( value )
+    lists.append(number)
+
+def sortList(): 
+    lists.sort()
+
+def popList(): 
+    lists.pop()
+
+def reverseList(): 
+    lists.reverse()
 
 if __name__ == '__main__':
     executeCommands()
--- domains/python/02-basic-data-types/01-lists/test_lists.py
+++ domains/python/02-basic-data-types/01-lists/test_lists.py
@@ -15,13 +15,45 @@ class TestLists(unittest.TestCase):
         actualOuput = self.process.communicate( input = '0\n' )[0]
         self.assertEquals('', actualOuput); 
 
-    def test_one_command(self):
-        actualOuput = self.process.communicate( input = '1\nprint\n' )[0]
-        self.assertEquals('[]\n', actualOuput); 
-
     def test_two_commands(self):
         actualOuput = self.process.communicate( input = '2\ninsert 0 5\nprint\n' )[0]
         self.assertEquals('[5]\n', actualOuput); 
+
+    def test_print(self):
+        actualOuput = self.process.communicate( input = '2\ninsert 0 5\nprint\n' )[0]
+        self.assertEquals('[5]\n', actualOuput); 
+
+    def test_insert(self):
+        actualOuput = self.process.communicate( input = '2\ninsert 0 5\nprint\n' )[0]
+        self.assertEquals('[5]\n', actualOuput); 
+
+    def test_remove(self):
+        actualOuput = self.process.communicate( input = '4\ninsert 0 5\ninsert 1 10\nremove 5\nprint\n' )[0]
+        self.assertEquals('[10]\n', actualOuput); 
+
+    def test_append(self):
+        actualOuput = self.process.communicate( input = '2\nappend 10\nprint\n' )[0]
+        self.assertEquals('[10]\n', actualOuput); 
+
+    def test_sort(self):
+        actualOuput = self.process.communicate( input = '3\nappend 10\nappend 5\nprint\n' )[0]
+        self.assertEquals('[10, 5]\n', actualOuput); 
+
+        process = subprocess.Popen(["python", "lists.py"], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
+        actualOuput = process.communicate( input = '4\nappend 10\nappend 5\nsort\nprint\n' )[0]
+        self.assertEquals('[5, 10]\n', actualOuput); 
+
+    def test_pop(self):
+        actualOuput = self.process.communicate( input = '3\nappend 10\npop\nprint\n' )[0]
+        self.assertEquals('[]\n', actualOuput); 
+
+    def test_reverse(self):
+        actualOuput = self.process.communicate( input = '4\nappend 10\nappend 5\nreverse\nprint\n' )[0]
+        self.assertEquals('[5, 10]\n', actualOuput); 
+
+    def test_case_1(self):
+        actualOuput = self.process.communicate( input = '12\ninsert 0 5\ninsert 1 10\ninsert 0 6\nprint\nremove 6\nappend 9\nappend 1\nsort\nprint\npop\nreverse\nprint\n' )[0]
+        self.assertEquals('[6, 5, 10]\n[1, 5, 9, 10]\n[9, 5, 1]\n', actualOuput); 
 
 if __name__ == '__main__':
     unittest.main()
