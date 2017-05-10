177ec346d556a6c45f04fd75395f4c8c78d7b996
Rizart Dokollari <r.dokollari@gmail.com>
2017-05-10 05:39:22 +0300

    Python: swap case.

---
+++ domains/python/03-strings/01-swap-case/test_swap_case.py
@@ -0,0 +1,27 @@
+import unittest
+import subprocess
+
+class TestSwapCase(unittest.TestCase):
+
+    def setUp(self):
+        self.process = subprocess.Popen( 
+            ["python", "swap_case.py"],
+            stdout=subprocess.PIPE, stdin=subprocess.PIPE, 
+            stderr=subprocess.STDOUT 
+        )
+
+    def tearDown(self):
+        self.process = None
+
+    def test_default_case(self):
+        actual_output = self.process.communicate( 
+            input = 'HackerRank.com presents "Pythonist 2".\n'
+        )[0]
+
+        self.assertEquals(
+            'hACKERrANK.COM PRESENTS "pYTHONIST 2".\n',
+            actual_output
+        ); 
+
+if __name__ == '__main__':
+    unittest.main()
