f4c8b3a8d16b0b57d73dff53feba6b71178e8b4b
Rizart Dokollari <r.dokollari@gmail.com>
2018-01-15 15:16:36 +0000

    Fix all failing tests.

--- domains/python/03-strings/09-whats-your-name/test_whats_your_name.py
+++ domains/python/03-strings/09-whats-your-name/test_whats_your_name.py
@@ -1,14 +1,24 @@
-import unittest
-from ddt import ddt, data, unpack
+import os
 import subprocess
+import unittest
+
+from ddt import ddt
+
 
 @ddt
 class TestWhatsYourName(unittest.TestCase):
-
     def test_greeting(self):
-        process = subprocess.Popen(["python", "whats_your_name.py"], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
-        actualOuput = process.communicate( input = 'James\nPotter\n' )[0]
-        self.assertEquals('Hello James Potter! You just delved into python.\n', actualOuput); 
+        script = os.path.dirname(
+            os.path.realpath(__file__)) + "/whats_your_name.py"
+
+        process = subprocess.Popen(["python", script],
+                                   stdout=subprocess.PIPE,
+                                   stdin=subprocess.PIPE,
+                                   stderr=subprocess.STDOUT)
+        actualOuput = process.communicate(input='James\nPotter\n')[0]
+        self.assertEquals('Hello James Potter! You just delved into python.\n',
+                          actualOuput);
+
 
 if __name__ == '__main__':
     unittest.main()
