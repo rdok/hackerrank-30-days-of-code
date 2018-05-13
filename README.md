00b48e435c109d2a07f7f19b9394a01caa578baf
Rizart Dokollari <r.dokollari@gmail.com>
2018-05-13 16:47:45 +0100

    Codeacademy: test hello world.

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-# HackerRank, the TDD way  
+# Learning Python, the TDD way  
+> Python katas from Hackerrank, CodeAcademy
 [![Build Status](https://travis-ci.org/rdok/hackerrank.com.svg?branch=master)](https://travis-ci.org/rdok/hackerrank.com)  
 
 
-> Python katas using test driven development.
-
- - Use PyCharm
- - Setup test configuration with pytest.
+ - Use PyCharm IDE
+ - Setup test configuration to use pytest.
  - On git bash execute: `python -m pip install -r requirements-to-freeze.txt`
---
+++ tests/codeacademy/learn_python/p01_python_syntax/p01_hello_world_test.py
@@ -0,0 +1,17 @@
+from src.codeacademy.learn_python.p01_python_syntax import p01_hello_world
+from tests.test_case import TestCase
+
+
+class TestScript(TestCase):
+
+    def setUp(self):
+        self.script = p01_hello_world.__file__
+
+    def test_it_returns_hello_world_string(self):
+        actual_output = self.execute(self.script)
+
+        expected = "Hello, world!\n"
+        expected += "Water-there is not a drop of water there! Were Niagara "
+        expected += "but a cataract of sand, would you travel your thousand miles to see it?\n"
+
+        self.assertEqual(expected, actual_output)
