93730ada92c66ce555b57355ca4b4bb4ab964d2f
Rizart Dokollari <r.dokollari@gmail.com>
2018-05-13 16:57:48 +0100

    Testing print string using + operator.

---
+++ src/codeacademy/learn_python/p01_python_syntax/p03_string.py
@@ -0,0 +1,1 @@
+print 'Hello ' + 'Chosen One'
---
+++ tests/codeacademy/learn_python/p01_python_syntax/p03_string_test.py
@@ -0,0 +1,15 @@
+from tests.test_case import TestCase
+from src.codeacademy.learn_python.p01_python_syntax import p03_string
+
+
+class StringTest(TestCase):
+
+    def setUp(self):
+        self.script =  p03_string.__file__
+
+    def test_print_string_using_plus_operator(self):
+        expected = 'Hello Chosen One\n'
+
+        actual = self.execute(self.script)
+
+        self.assertEqual(expected, actual)
