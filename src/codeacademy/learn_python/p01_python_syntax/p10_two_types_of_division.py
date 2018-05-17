b22e20937a3a114e72270a1127f95f76350211b7
Rizart Dokollari <r.dokollari@gmail.com>
2018-05-17 02:17:20 +0100

    TwoTypesOfDivisionTest: add tests. See description. - test_it_holds_float_cucumbers_per_person - test_it_prints_float_cucumbers_per_person

--- src/codeacademy/learn_python/p01_python_syntax/p10_two_types_of_division.py
+++ src/codeacademy/learn_python/p01_python_syntax/p10_two_types_of_division.py
@@ -1,4 +1,7 @@
 cucumbers = 100
 num_people = 6
 whole_cucumbers_per_person = cucumbers / num_people
+float_cucumbers_per_person = float(cucumbers) / num_people
+
 print whole_cucumbers_per_person
+print float_cucumbers_per_person
--- tests/codeacademy/learn_python/p01_python_syntax/p10_two_types_of_division.py
+++ tests/codeacademy/learn_python/p01_python_syntax/p10_two_types_of_division.py
@@ -7,7 +7,8 @@ class TwoTypesOfDivisionTest(TestCase):
         self.script = p10_two_types_of_division
         self.cucumbers = 100
         self.num_people = 6
-        self.whole_cucumbers_per_person = self.cucumbers / self.num_people
+        self.whole_cucumbers_per_person = 16
+        self.float_cucumbers_per_person = 16.666666666666668
 
     def test_it_holds_cucumbers_and_number_of_people(self):
         self.assertIsInstance(self.script.cucumbers, int)
@@ -23,6 +24,17 @@ class TwoTypesOfDivisionTest(TestCase):
         )
 
     def test_it_prints_whole_cucumbers_per_person(self):
-        printed_response = self.execute(self.script.__file__)
-        expected_response = str(self.whole_cucumbers_per_person) + "\n"
-        self.assertEqual(expected_response, printed_response)
+        container = self.execute(self.script.__file__)
+        member = str(self.whole_cucumbers_per_person)
+        self.assertIn(member, container)
+
+    def test_it_holds_float_cucumbers_per_person(self):
+        self.assertAlmostEqual(
+            self.float_cucumbers_per_person,
+            self.script.float_cucumbers_per_person,
+        )
+
+    def test_it_prints_float_cucumbers_per_person(self):
+        container = self.execute(self.script.__file__)
+        member = str(self.float_cucumbers_per_person)
+        self.assertIn(member, container)
